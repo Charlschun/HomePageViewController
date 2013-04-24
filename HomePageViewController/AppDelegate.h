d37d45fa8f06dea39a42a7b9fb18611b3fa7cb07
Charls <beiqiang22@163.com>
2013-04-24 16:20:41 +0800

    first

---
+++ HomePageViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HomePageViewController
+//
+//  Created by sskh on 13-3-28.
+//  Copyright (c) 2013年 sskh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HomePageViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HomePageViewController
+//
+//  Created by sskh on 13-3-28.
+//  Copyright (c) 2013年 sskh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
